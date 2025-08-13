c44fe59e2fde0646f126d937a43e7e217c2b5b25
Alejandro Ruiz <alejandroruizsumelzo@gmail.com>
2025-08-13 13:07:04 +0200

    upload vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlify()],
+  build: {
+    outDir: "dist",
+  },
 });
