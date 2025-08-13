e45552a9e96f467d4b93787ec487fdc1ffe96478
Alejandro Ruiz <alejandroruizsumelzo@gmail.com>
2025-08-13 13:23:45 +0200

    Add tailwind to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 
-import netlify from "@netlify/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlify()],
-  build: {
-    outDir: "dist",
-  },
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyPlugin() as any,
+  ],
 });
